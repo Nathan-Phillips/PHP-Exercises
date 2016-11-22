5e2d13914c0a33281d22f8d6c36a676deddcb6a6
Nathan-Phillips <philip.nobo@gmail.com>
2016-11-22 12:45:49 +0000

    arrays
    New exercises using arrays

---
+++ exercises_arrays.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>PHP Exercises with Arrays</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:400,600,700" rel="stylesheet">
+        <link href="css/normalise.css" rel="stylesheet" media="screen" />
+        <link href="css/exercises.css" rel="stylesheet" media="screen" />
+    </head>
+    <body>
+
+        <section>Exercise 01</section>
+        <p>$colour = array('white', 'green', 'red', 'blue', 'black');<br>Write a script which will display the sentence below where the words 'red', 'green' and 'white' will come from $colour.</p>
+        <div class="answer">
+            
+        <?php
+            $colour = array('white', 'green', 'red', 'blue', 'black');    
+            
+            echo 'The memory of that scene for me is like a frame of film forever frozen at that moment: the ' . $colour[2] . ' carpet, the ' . $colour[1] . ' lawn, the ' . $colour[0] . ' house, the leaden sky. The new president and his first lady. - Richard M. Nixon.';
+        ?>
+        </div>
+
+        <section>Exercise 02</section>
+        <p>$colour = array('white', 'green', 'red'). Write a PHP script which will display the colors in the following way : 
+            <br>white, green, red,
+            <br>• green
+            <br>• red
+            <br>• white
+        </p>
+        <div class="answer">
+            
+        <?php       
+            $colour = array('white', 'green', 'red');    
+            
+            foreach($colour as $col){
+                echo $col . ', ';
+            }
+
+            sort($colour);
+
+            echo '<ul>';
+                foreach($colour as $alpha){
+                    echo '<li>' . $alpha . '</li>';
+                }
+            echo '</ul>';
+
+        ?>
+        </div>
+
+        <section>Exercise 03</section>
+        <p>Create a PHP script which display the capital and country name from the array $ceu. Sort the list by the capital of the country.</p>
+        <div class="answer">
+            
+        <?php       
+            $ceu = array( 'Italy'=>'Rome', 'Luxembourg'=>'Luxembourg', 'Belgium'=> 'Brussels', 'Denmark'=>'Copenhagen', 'Finland'=>'Helsinki', 'France' => 'Paris', 'Slovakia'=>'Bratislava', 'Slovenia'=>'Ljubljana', 'Germany' => 'Berlin', 'Greece' => 'Athens', 'Ireland'=>'Dublin', 'Netherlands'=>'Amsterdam', 'Portugal'=>'Lisbon', 'Spain'=>'Madrid', 'Sweden'=>'Stockholm', 'United Kingdom'=>'London', 'Cyprus'=>'Nicosia', 'Lithuania'=>'Vilnius', 'Czech Republic'=>'Prague', 'Estonia'=>'Tallin', 'Hungary'=>'Budapest', 'Latvia'=>'Riga', 'Malta'=>'Valetta', 'Austria' => 'Vienna', 'Poland'=>'Warsaw') ;
+        
+            asort($ceu);
+
+            foreach($ceu as $key => $value){
+                echo 'The capital of ' . $key . ' is ' . $value . '.<br>';
+            }
+
+        ?>
+        </div>
+
+    </body>
+</html>
